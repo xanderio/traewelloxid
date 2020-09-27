dc5b266c679a4db9ccc3518e530bcca0d05cd303
Alexander Sieg <alex@xanderio.de>
2020-09-27 18:41:53 +0200

    implemented all types include in the status_paginate

--- src/types.rs
+++ src/types.rs
@@ -16,8 +16,95 @@ pub enum ApiError {
     NotAcceptable406,
 }
 
+pub struct StatusPaginate {
+    current_page: u64,
+    data: Vec<Status>,
+    first_page_url: String,
+    from_page_url: String,
+    from: u64,
+    next_page_url: String,
+    path: String,
+    per_page: u64,
+    prev_page_url: String,
+    to: u64,
+}
+
 #[derive(Deserialize, Debug, Clone)]
-pub struct Status {}
+pub struct Status {
+    id: u64,
+    created_at: String,
+    updated_at: String,
+    body: String,
+    #[serde(rename = "type")]
+    status_type: String,
+    link_count: String,
+    liked: bool,
+    user: User,
+    train_checkin: TrainCheckin,
+    event: Option<Event>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct User {
+    id: u64,
+    name: String,
+    username: String,
+    train_distance: f64,
+    train_duration: u64,
+    points: u64,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct TrainCheckin {
+    id: u64,
+    status_id: u64,
+    trip_id: String,
+    origin: Station,
+    destintion: Station,
+    distance: u64,
+    departure: String,
+    arrival: String,
+    points: u64,
+    delay: u64,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Station {
+    id: u64,
+    ibnr: String,
+    name: String,
+    latiude: f32,
+    longitude: f32,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct HafasTrip {
+    id: u64,
+    trip_id: String,
+    category: String,
+    number: String,
+    linename: String,
+    origin: String,
+    destination: String,
+    stopovers: String,
+    polyline: String,
+    departure: String,
+    arrival: String,
+    delay: u64,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Event {
+    id: u64,
+    name: String,
+    slug: String,
+    hashtag: String,
+    host: String,
+    url: String,
+    trainstation: String,
+    begin: String,
+    end: String,
+}
 
 #[derive(Deserialize, Debug, Clone)]
 pub enum View {
